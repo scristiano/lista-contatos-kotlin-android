6b151afffd19974b99125ebaac328f89caca9de3
Cristiano Schaarschmidt <cristiano.schaarschmidt@gmail.com>
2021-04-08 22:30:24 -0300

    Aplicativo Android desenvolvido em Kotlin que permite a inserção, atualização e excução de contatos em uma lista. Os dados são persistidos em um banco SQLite.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lista de Contatos em Kotlin / Android Studio
+
+## Descrição
+
+Aplicativo Android desenvolvido em Kotlin que permite a inserção, atualização e excução de contatos em uma lista.
+
+Os dados são persistidos em um banco SQLite.
+
+Tela Inicial
+
+![Tela Inicial](./images/01.png)
+
+Tela de Consulta e Atualização
+
+![Tela de Consulta e Atualização](./images/03.png)
+
+Tela de Cadastro de Contatos
+
+![Tela de Cadastro de Contatos](./images/02.png)
---
+++ app/src/main/java/com/everis/listadecontatos/helpers/HelperDB.kt
@@ -0,0 +1,131 @@
+package com.everis.listadecontatos.helpers
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.everis.listadecontatos.feature.listacontatos.model.ContatosVO
+
+class HelperDB(
+    context: Context
+) : SQLiteOpenHelper(context, NOME_BANCO, null, VERSAO_ATUAL) {
+
+    companion object {
+        private val NOME_BANCO = "contato.db"
+        private val VERSAO_ATUAL = 2
+    }
+
+    val TABLE_NAME = "contato"
+    val COLUMNS_ID = "id"
+    val COLUMNS_NOME = "nome"
+    val COLUMNS_TELEFONE = "telefone"
+    val DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
+    val CREATE_TABLE = "CREATE TABLE $TABLE_NAME(" +
+            "$COLUMNS_ID INTEGER NOT NULL," +
+            "$COLUMNS_NOME TEXT NOT NULL," +
+            "$COLUMNS_TELEFONE TEXT NOT NULL," +
+            "" +
+            "PRIMARY KEY($COLUMNS_ID AUTOINCREMENT)" +
+            ")"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CREATE_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        if (oldVersion != newVersion){
+            //update do banco
+            db?.execSQL(DROP_TABLE)
+        }
+        onCreate(db)
+    }
+
+    fun buscarContatos(busca: String, isBuscaPorID: Boolean = false) : List<ContatosVO> {
+        //salvarContato(ContatosVO(0,"TESTE 3","TESTE 3"))
+        val db = readableDatabase ?: return mutableListOf()
+        var lista = mutableListOf<ContatosVO>()
+
+        //Maneira 1 de fazer o SELECT
+        //val sql = "SELECT * FROM $TABLE_NAME WHERE $COLUMNS_NOME LIKE '%$busca%'"
+        // val sql = "SELECT * FROM $TABLE_NAME WHERE $COLUMNS_NOME LIKE ?"
+        //val buscaComPercentual = "%$busca%"
+        //var cursor = db.rawQuery(sql, arrayOf())
+        //var cursor = db.rawQuery(sql, arrayOf(buscaComPercentual))
+
+        //Maneira 2 de fazer o SELECT
+        var where: String? = null
+        var args: Array<String> = arrayOf()
+        if(isBuscaPorID){
+            where = "$COLUMNS_ID = ?"
+            args = arrayOf("$busca")
+        }else{
+            where = "$COLUMNS_NOME LIKE ?"
+            args = arrayOf("%$busca%")
+        }
+        var cursor = db.query(TABLE_NAME,null,where,args,null,null,null)
+        if (cursor == null){
+            db.close()
+            return mutableListOf()
+        }
+        while (cursor.moveToNext()){
+            var contato = ContatosVO(
+                cursor.getInt(cursor.getColumnIndex(COLUMNS_ID)),
+                cursor.getString(cursor.getColumnIndex(COLUMNS_NOME)),
+                cursor.getString(cursor.getColumnIndex(COLUMNS_TELEFONE))
+            )
+            lista.add(contato)
+        }
+        db.close()
+        return lista
+    }
+
+    fun salvarContato(contato : ContatosVO){
+        val db = writableDatabase ?: return
+
+        //Maneira 1 de fazer o INSERT
+        //val sql = "INSERT INTO $TABLE_NAME ($COLUMNS_NOME, $COLUMNS_TELEFONE) VALUES (?,?)"
+        //var array = arrayOf(contato.nome,contato.telefone)
+
+        //Maneira 2 de fazer o INSERT
+        var content = ContentValues()
+        content.put(COLUMNS_NOME,contato.nome)
+        content.put(COLUMNS_TELEFONE,contato.telefone)
+        db.insert(TABLE_NAME,null,content)
+        //db.execSQL(sql,array)
+        db.close()
+    }
+
+    fun deletarContato(id: Int){
+        val db = writableDatabase ?: return
+
+        //Maneira 1 de fazer o DELETE
+        //val where = "id = ?"
+        //val arg = arrayOf("$id")
+        //db.delete(TABLE_NAME,where,arg)
+
+        //Maneira 2 de fazer o DELETE
+        val sql = "DELETE FROM $TABLE_NAME WHERE $COLUMNS_ID= ?"
+        val arg = arrayOf("$id")
+        db.execSQL(sql,arg)
+        db.close()
+    }
+
+    fun updateContato(contato: ContatosVO){
+        val db = writableDatabase ?: return
+
+        //Maneira 1 de fazer o UPDATE
+        //val content = ContentValues()
+        //content.put(COLUMNS_NOME,contato.nome)
+        //content.put(COLUMNS_TELEFONE,contato.telefone)
+        //val where = "id = ?"
+        //val arg = arrayOf("${contato.id}")
+        //db.update(TABLE_NAME,content,where,arg)
+
+        //Maneira 2 de fazer o UPDATE
+        val sql = "UPDATE $TABLE_NAME SET $COLUMNS_NOME = ?, $COLUMNS_TELEFONE = ? WHERE $COLUMNS_ID = ?"
+        val arg = arrayOf(contato.nome, contato.telefone, contato.id)
+        db.execSQL(sql,arg)
+        db.close()
+    }
+
+}
